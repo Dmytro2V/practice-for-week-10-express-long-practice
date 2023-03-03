68432a3ef29968da7b7cc77735b9e39033c2d58f
Dmytro2V <op2vin@gmail.com>
2023-03-04 01:42:27 +0200

    done phase1 + phase 2

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.use('/test-json', express.json()) // task 2 - need to parse body from request
+app.use('/test-json', express.json()) // Phase 1 task 2 - need to parse body from request
 
+
+// Phase 2 task 1Logger Middleware
+// Create a middleware function that will log the method and URL path of the request to the terminal 
+//for all requests to the server.
+app.use((req, res, next) => {
+  console.log('request method: ', req.method);
+  console.log('request URL: ', req.url);
+  res.on('finish', () => { // Phase 2 task2 - responce code on event listener (when ready)
+    // read and log the status code of the response
+    console.log(res.statusCode);
+  });
+  next();
+})
+app.use('/static' , express.static('assets')); // Phase 1 task 1 - for static 
 // For testing purposes, GET /
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
@@ -20,13 +34,17 @@ app.post('/test-json', (req, res, next) => {
 // For testing express-async-errors
 app.get('/test-error', (req, res) => {
   throw new Error("Hello World!")
-  next(err);                       // task 3 - for asynhronous functions next(err) is must!
+  next(err);                       // Phase 1 task 3 - for asynhronous functions next(err) is must!
 });
 
 
-app.use('/static' , express.static('assets')); // task 1 - for static 
 
-app.use((err, req, res, next) => {
+//Phase 2 task 3 - Resource Not Found
+app.use( (req, res) => {  
+  res.statusCode = 404;
+  throw new Error("The requested resource couldn't be found.")
+});
+app.use((err, req, res, next) => { // Phase 1 task 3 error listener
   res.json("Error: " + err.message);
 });
 
